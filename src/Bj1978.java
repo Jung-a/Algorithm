d645d9179088e08053a79e41c736b7f9b1ae13d4
junga <gop3064@naver.com>
2020-01-13 03:05:07 +0900

    백준 1978번 소수 찾기

--- src/Bj1978.java
+++ src/Bj1978.java
@@ -1,19 +1,26 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+// 1978번 소수 찾기
+// https://www.acmicpc.net/problem/1978
 public class Bj1978 {
 
-    // 소수를 미리 구해놓기 14372KB, 112ms
     public static void main(String[] args) {
+//        solution1();
+//        solution2();
+        solution3();
+    }
+
+    // 주어진 수의 최대값만큼의 소수값만 구해놓기 14364KB, 112ms
+    public static void solution3() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // n <= 100
         int[] val = new int[n];
         int max = 0;
         for(int i = 0; i < n; i++) {
             val[i] = sc.nextInt(); // val <= 1,000
-            if (max < val[i]) max = val[i];
+            if (max < val[i]) max = val[i]; // 최댓값 구하기
         }
-
         boolean[] arr = new boolean[max+1];
         Arrays.fill(arr, true);
         arr[0] = arr[1] = false;
@@ -28,14 +35,13 @@ public class Bj1978 {
         }
         int answer = 0;
         for(int i = 0; i < n; i++) {
-            System.out.println(arr[i]);
-            if (arr[i]) answer++;
+            if (arr[val[i]]) answer++;
         }
         System.out.println(answer);
     }
 
     // 소수를 미리 구해놓기 14372KB, 112ms
-    public static void main2(String[] args) {
+    public static void solution2() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // n <= 100
         boolean[] arr = new boolean[1001];
@@ -59,7 +65,7 @@ public class Bj1978 {
     }
 
     // 값을 입력받을 때마다 구하기 14376KB, 116ms
-    public static void main1(String[] args) {
+    public static void solution1() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // n <= 100
         int answer = 0;
